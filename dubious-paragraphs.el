c8750ef41d36d6e1944ebf133ebb0128ac529005
Jonas Bernoulli <jonas@bernoul.li>
2018-02-24 18:51:03 +0100

    updates

--- dubious-paragraphs.el
+++ dubious-paragraphs.el
@@ -91,6 +91,22 @@
 ;;;   just a question of setting the appropriate paragraph-* variables or we
 ;;;   might be able to change the functions themselves.
 ;;;
+;;; License
+;;; =======
+;;;
+;;; This file is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3, or (at your option)
+;;; any later version.
+;;;
+;;; This file is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;
 ;;; END
 ;;;
 ;;; Code:
